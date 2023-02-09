d199931d796e8326daa2911a0a0aa44ca5c698e6
久野文菜 <ayana.kuno@mixi.co.jp>
2023-02-09 13:29:56 +0900

    camera 表示

--- lib/stack/stack_example.dart
+++ lib/stack/stack_example.dart
@@ -1,9 +1,38 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-class StackExample extends StatelessWidget {
+class StackExample extends StatefulWidget {
   const StackExample({
     Key? key,
+    required this.camera,
   }) : super(key: key);
+
+  final CameraDescription camera;
+
+  @override
+  StackExampleState createState() => StackExampleState();
+}
+
+class StackExampleState extends State<StackExample> {
+  late CameraController _cameraController;
+
+  @override
+  void initState() {
+    _cameraController = CameraController(
+      // カメラを指定
+      widget.camera,
+      // 解像度を定義
+      ResolutionPreset.high,
+    );
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,41 +40,31 @@ class StackExample extends StatelessWidget {
       appBar: AppBar(
         title: const Text("AppBar"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Stack(
-          fit: StackFit.expand,
-          children: [
-            SizedBox(
-              height: 100,
-              width: 100,
-              child: ColoredBox(color: Colors.cyan.withAlpha(40)),
+      body: FutureBuilder(
+        future: _cameraController.initialize(),
+        builder: (context, snapshot) {
+          return Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Center(
+              child: Stack(
+                children: [
+                  CameraPreview(_cameraController),
+                  SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: ColoredBox(color: Colors.cyan.withAlpha(40)),
+                  ),
+                  SizedBox(
+                    height: 50,
+                    width: 150,
+                    child: ColoredBox(color: Colors.amber.withAlpha(40)),
+                  ),
+                ],
+              ),
             ),
-            SizedBox(
-              height: 50,
-              width: 150,
-              child: ColoredBox(color: Colors.amber.withAlpha(40)),
-            ),
-            CustomPaint(
-              painter: _SamplePainter(),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
-  }
-}
-
-class _SamplePainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()..color;
-    paint.color = Colors.red.withAlpha(40);
-    canvas.drawCircle(const Offset(0, 0), 25, paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
   }
 }
