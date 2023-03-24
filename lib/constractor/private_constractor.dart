7cbc18a2125fda35e2d6fb6a625f3d3a43ffdc0e
久野文菜 <ayana.kuno@mixi.co.jp>
2023-03-24 16:42:29 +0900

    Private Constructor

---
+++ lib/constractor/private_constractor.dart
@@ -0,0 +1,18 @@
+/* Private Constructor */
+void main() {
+  // _ つけると外から呼べない！
+  // final senior = Account('シニア',100);
+
+  // 同じファイル内で _ をつけると（Named Constractor として）呼び出せる
+  final senior = Account._('シニア', 100);
+
+  print(senior.name);
+}
+
+class Account {
+  // final や required つけない
+  String name;
+  int age;
+
+  Account._(this.name, this.age);
+}
